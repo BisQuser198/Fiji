9fe070fb71616cbb6400a3397846d24f5e5ee2d3
BisQUser198 <clmishto3@yahoo.com>
2025-10-21 13:18:32 +0300

    added admin access to table

--- CS_game/admin.py
+++ CS_game/admin.py
@@ -1,3 +1,11 @@
+# CS_game/admin.py
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+# exposes the Employee table in admin & configures its display / list columns, search, filters
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'department_id', 'manager_id', 'salary', 'promotable')  # columns shown in list view
+    search_fields = ('name',)       # quick search box
+    list_filter = ('promotable',)   # sidebar filter
+# Now you can manage Employee records via Django admin interface
