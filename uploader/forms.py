5a03c506ffa58f81252bd63a3882f88e6a82526f
BisQUser198 <clmishto3@yahoo.com>
2025-10-24 12:35:32 +0300

    details added on Uploader

--- uploader/forms.py
+++ uploader/forms.py
@@ -2,3 +2,5 @@ from django import forms
 
 class UploadForm(forms.Form):
     file = forms.FileField(label='Select a file')
+# forms.FileField creates an HTML <input type="file"> element
+# forms.FileField wraps the file content in an UploadedFile object when the form is submitted
--- uploader/views.py
+++ uploader/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Upload
+from .models import Upload # Upload class objects store the files
 from .forms import UploadForm
 from .utils import process_file
 
-def upload_file(request):
+def upload_file(request): # accept incoming HTTP request and returns redirect
     # Display empty form / handle POST
-    if request.method == 'POST':
-        form = UploadForm(request.POST, request.FILES)
-        if form.is_valid():
+    if request.method == 'POST': # enter POST branch only when client submits form / data
+        form = UploadForm(request.POST, request.FILES) # instantiate UploadForm (forms.FileField) with POST data 
+        if form.is_valid(): # request.FILES contains uploaded files
             Upload.objects.create(file=form.cleaned_data['file'])
             return redirect('uploader:upload_list')
     else:
-        form = UploadForm()
+        form = UploadForm() # instantiate empty form for GET request
     return render(request, 'uploader/upload.html', {'form': form})
+# render syntax: render(request, template_name, context) -- context is a dict mapping template variable names to Python objects
+# form variable calls UploadForm() - empty object
 
 def upload_list(request):
     # List all uploaded files
